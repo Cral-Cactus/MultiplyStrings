8f8afbb8c97d09dd9b3a7db4053ff37620a4e5e4
Cral_Cactus <marian.pb@abv.bg>
2023-03-21 08:25:32 +0200

    Uplaod

---
+++ MultiplyStrings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MultiplyStrings
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string[] inputArr = input.Split(' ');
+
+            string str1 = inputArr[0];
+            string str2 = inputArr[1];
+
+            int multiply = MultiplyStrings(str1, str2);
+            Console.WriteLine(multiply);
+        }
+
+        static int MultiplyStrings(string str1, string str2)
+        {
+            int multiply = 0;
+            int length = Math.Max(str1.Length, str2.Length);
+
+            for (int i = 0; i < length; i++)
+            {
+                int charCode1 = i < str1.Length ? (int)str1[i] : 1;
+                int charCode2 = i < str2.Length ? (int)str2[i] : 1;
+
+                multiply += charCode1 * charCode2;
+            }
+
+            return multiply;
+        }
+
+    }
+}
